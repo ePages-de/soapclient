cfd077781c9f56cdadfdd574a9394bc7c2b93815
Oliver Trosien <otrosien@epages.com>
2013-11-13 13:11:48 +0100

    fix output directory of generate scripts

--- generateStubs.sh
+++ generateStubs.sh
@@ -6,7 +6,7 @@
 
 
 export CLASSPATH=lib/axis.jar:lib/commons-logging-1.0.4.jar:lib/commons-discovery-0.2.jar:lib/jaxrpc.jar:lib/saaj.jar:lib/wsdl4j-1.5.1.jar
-WSDL2JAVA='java org.apache.axis.wsdl.WSDL2Java -o src'
+WSDL2JAVA='java org.apache.axis.wsdl.WSDL2Java -o src/main/java'
 echo $WSDL2JAVA
 
 
