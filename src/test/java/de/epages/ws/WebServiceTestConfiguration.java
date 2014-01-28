01eb88412536fd585e359901e87fb3adfc30c835
Oliver Trosien <otrosien@epages.com>
2014-01-28 23:31:29 +0100

    Be specific on NPE in TestConfiguration

--- src/test/java/de/epages/ws/WebServiceTestConfiguration.java
+++ src/test/java/de/epages/ws/WebServiceTestConfiguration.java
@@ -14,18 +14,18 @@ import de.epages.ws.WebServiceConfiguration;
 
 /**
  * Common configuration data for all web services.
- * 
- * Tries to resolve an epages6 hostname by
- * - reading System property "ep6HostName"
- * - reading epages.conf for value "SystemDomainName"
- * - using a fallback on "localhost".
- * 
+ *
+ * Tries to resolve an epages6 hostname by:
+ *  - reading System property "ep6HostName"
+ *  - reading epages.conf for value "SystemDomainName"
+ *  - using a fallback on "localhost".
+ *
  * Default credentials for DemoShop admin are used to authenticate the user.
  */
 public class WebServiceTestConfiguration implements WebServiceConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger(WebServiceTestConfiguration.class);
-    
+
     public final static String WEBSERVICE_LOGIN = "/Shops/DemoShop/Users/admin";
     public final static String WEBSERVICE_PASSWORD = "admin";
     public final static URL WEBSERVICE_URL = new WebServiceTestConfiguration().doGetWebserviceURL();
@@ -60,16 +60,16 @@ public class WebServiceTestConfiguration implements WebServiceConfiguration {
 
     private String getHostnameFromEpagesConf() {
         String confPath = System.getenv("EPAGES_CONFIG");
-        if (!confPath.isEmpty()) {
+        if (confPath != null) {
             Properties p = new Properties();
-                try {
-                    p.load(new FileInputStream(confPath + "/epages.conf"));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                return p.getProperty("SystemDomainName");
+            try {
+                p.load(new FileInputStream(confPath + "/epages.conf"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return p.getProperty("SystemDomainName");
         }
-        return null;
+        throw new NullPointerException("EPAGES_CONFIG");
     }
 
     private URL doGetWebserviceURL() {
