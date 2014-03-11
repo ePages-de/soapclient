e7e69d8b3af1d41c14b4d0cc472bf9390bffd14f
Oliver Trosien <otrosien@epages.com>
2014-03-11 16:14:38 +0100

    migrated orderdocumentservice3

---
+++ src/test/java/de/epages/ws/orderdocument3/OrderDocumentServiceClient.java
@@ -0,0 +1,65 @@
+package de.epages.ws.orderdocument3;
+
+import de.epages.ws.orderdocument3.model.TCreate_Return;
+import de.epages.ws.orderdocument3.model.TDelete_Return;
+import de.epages.ws.orderdocument3.model.TExists_Return;
+import de.epages.ws.orderdocument3.model.TGetCreditNotes_Return;
+import de.epages.ws.orderdocument3.model.TGetInfo_Return;
+import de.epages.ws.orderdocument3.model.TGetInvoices_Return;
+import de.epages.ws.orderdocument3.model.TGetPackingSlips_Return;
+import de.epages.ws.orderdocument3.stub.Port_OrderDocument;
+
+public interface OrderDocumentServiceClient extends Port_OrderDocument {
+
+    /**
+     * get information about a list of order documents
+     */
+    TGetInfo_Return[] getInfo(String[] orderDocuments);
+
+    /**
+     * get information about a list of order documents
+     */
+    TGetInfo_Return[] getInfo(String[] orderDocuments, String[] attributes);
+
+    /**
+     * get information about a list of order documents
+     */
+    TGetInfo_Return[] getInfo(String[] orderDocuments, String[] attributes, String[] addressAttributes);
+
+    /**
+     * get information about a list of order documents
+     */
+    TGetInfo_Return[] getInfo(String[] orderDocuments, String[] attributes, String[] addressAttributes, String[] languageCodes);
+
+    /**
+     * check if a list of order documents exist.
+     */
+    TExists_Return[] exists(String[] orderDocuments);
+
+    /**
+     * delete a list of order documents
+     */
+    TDelete_Return[] delete(String[] orderDocuments);
+
+    /**
+     * create new order documents
+     */
+    TCreate_Return[] create(de.epages.ws.orderdocument3.model.TCreate_Input[] orderDocuments);
+
+    /**
+     * get invoices of an order
+     */
+    TGetInvoices_Return[] getInvoices(String[] orders);
+
+    /**
+     * get packing slips of an order
+     */
+    TGetPackingSlips_Return[] getPackingSlips(String[] orders);
+
+    /**
+     * get credit notes of an order
+     */
+    TGetCreditNotes_Return[] getCreditNotes(String[] orders);
+
+
+}
