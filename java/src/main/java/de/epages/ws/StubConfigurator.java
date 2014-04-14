0c75efa348b59a18aae73046485234632f62cc0c
Oliver Trosien <otrosien@epages.com>
2014-04-14 11:46:22 +0200

    be compatible with java 1.6

--- java/src/main/java/de/epages/ws/StubConfigurator.java
+++ java/src/main/java/de/epages/ws/StubConfigurator.java
@@ -9,7 +9,7 @@ import org.apache.axis.transport.http.HTTPConstants;
 public class StubConfigurator {
 
     public static <T extends Stub> T configure(T stub, WebServiceConfiguration config) {
-        Hashtable<String, String> headers = new Hashtable<>();
+        Hashtable<String, String> headers = new Hashtable<String, String>();
         headers.put(HTTPConstants.HEADER_TRANSFER_ENCODING_CHUNKED, Boolean.FALSE.toString());
         String credentials = config.getUsername() + ":" + config.getPassword();
         String authHeader = "Basic " + Base64.encode(credentials.getBytes());
