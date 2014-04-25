9ae52d97e8897827bb4e167d5a0611bc7d97c6c8
Oliver Trosien <otrosien@epages.com>
2014-04-25 22:34:34 +0200

    migrated shopconfigservice5

---
+++ java/provider/src/gen-old/java/de/epages/ws/shopconfig5/stub/ShopConfigService.java
@@ -0,0 +1,20 @@
+/**
+ * ShopConfigService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package de.epages.ws.shopconfig5.stub;
+
+public interface ShopConfigService extends javax.xml.rpc.Service {
+
+/**
+ * ePages ShopConfig Service
+ */
+    public java.lang.String getport_ShopConfigAddress();
+
+    public de.epages.ws.shopconfig5.stub.Port_ShopConfig getport_ShopConfig() throws javax.xml.rpc.ServiceException;
+
+    public de.epages.ws.shopconfig5.stub.Port_ShopConfig getport_ShopConfig(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
---
+++ java/provider/src/test/java/de/epages/ws/shopconfig5/ShopConfigServiceStubFactory.java
@@ -0,0 +1,8 @@
+package de.epages.ws.shopconfig5;
+
+import de.epages.ws.WebServiceConfiguration;
+import de.epages.ws.shopconfig5.stub.*;
+
+public interface ShopConfigServiceStubFactory {
+    Port_ShopConfig create(WebServiceConfiguration config, ShopConfigService service);
+}
