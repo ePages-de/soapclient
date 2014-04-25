ec9c92d1532b542f237309ca2b92adf31af77d58
Oliver Trosien <otrosien@epages.com>
2014-04-25 22:40:37 +0200

    migrated shopconfigservice6

---
+++ java/provider/src/test/java/de/epages/ws/shopconfig6/ShopConfigServiceStubFactoryImpl.java
@@ -0,0 +1,27 @@
+package de.epages.ws.shopconfig6;
+
+import javax.xml.rpc.ServiceException;
+
+import de.epages.ws.StubConfigurator;
+import de.epages.ws.WebServiceConfiguration;
+import de.epages.ws.shopconfig6.stub.ShopConfigService;
+import de.epages.ws.shopconfig6.stub.Bind_ShopConfig_SOAPStub;
+import de.epages.ws.shopconfig6.stub.Port_ShopConfig;
+
+final class ShopConfigServiceStubFactoryImpl implements ShopConfigServiceStubFactory {
+
+    @Override
+    public Port_ShopConfig create(WebServiceConfiguration config, ShopConfigService service) {
+
+        try {
+            Bind_ShopConfig_SOAPStub stub = (Bind_ShopConfig_SOAPStub) service.getport_ShopConfig(config.getWebserviceURL());
+            if (stub == null) {
+                throw new NullPointerException("stub");
+            }
+            return StubConfigurator.configure(stub, config);
+        } catch (ServiceException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
