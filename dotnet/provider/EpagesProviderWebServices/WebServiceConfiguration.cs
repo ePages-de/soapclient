29a1c5a9f10a6359d74d9f4fc2858e91a51f5739
Oliver Trosien <otrosien@epages.com>
2014-04-26 17:32:48 +0200

    dotnet: fixed configuration of epages6 hostname

--- dotnet/provider/EpagesProviderWebServices/WebServiceConfiguration.cs
+++ dotnet/provider/EpagesProviderWebServices/WebServiceConfiguration.cs
@@ -1,3 +1,6 @@
+using System;
+using STA.Settings;
+
 namespace EpagesProviderWebServices
 {
 	/// <summary>
@@ -5,8 +8,32 @@ namespace EpagesProviderWebServices
 	/// </summary>
 	public class WebServiceConfiguration
 	{
-		public const string WEBSERVICE_URL = "http://localhost:8080/epages/Site.soap";
+
+		public string WEBSERVICE_SERVER;
+		public string WEBSERVICE_URL;
 		public const string WEBSERVICE_LOGIN = "/Providers/Distributor/Users/admin";
 		public const string WEBSERVICE_PASSWORD = "admin";
+
+		public WebServiceConfiguration()
+		{
+			WEBSERVICE_SERVER = getWServerFromConfig();
+			//use explicit servername and port for tracing
+			//WEBSERVICE_SERVER = "hmoye:8080";
+			WEBSERVICE_URL = "http://" + WEBSERVICE_SERVER + "/epages/Site.soap";
+		}
+
+		public String getWServerFromConfig()
+		{
+			// set a fixed hostname via environment parameters.
+			String epHostName = System.Environment.GetEnvironmentVariable("ep6HostName");
+			if (epHostName != null) {
+				return epHostName;
+			}
+			String epConfDir = System.Environment.GetEnvironmentVariable("EPAGES_CONFIG");
+			INIFile epConf = new INIFile(epConfDir + "\\epages.conf");
+			String Server = epConf.GetValue("DE_EPAGES::Object", "SystemDomainName", "localhost");
+			return Server;
+		}
+
 	}
 }
