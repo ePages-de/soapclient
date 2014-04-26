b65c2327625041fa1deaad0feb30060cd0eeb3a4
Oliver Trosien <otrosien@epages.com>
2014-04-26 15:50:06 +0200

    migrate shopconfigservice7

---
+++ java/provider/src/main/java/de/epages/ws/shopconfig7/ShopConfigServiceClient.java
@@ -0,0 +1,49 @@
+package de.epages.ws.shopconfig7;
+
+import de.epages.ws.shopconfig7.stub.*;
+
+public interface ShopConfigServiceClient extends Port_ShopConfig {
+
+    /**
+     * creates a new shop with a given shop type and imports default
+     * data
+     */
+    int create(TCreateShop createShopData);
+
+    /**
+     * deletes a shop from the store database
+     */
+    void delete(TShopRef shopRef);
+
+    /**
+     * returns true if the shop alias exists for the provider
+     */
+    boolean exists(TShopRef shopRef);
+
+    /**
+     * updates an existing shop in the site and store database
+     */
+    void update(TUpdateShop shopData);
+
+    /**
+     * returns information about an existing shop from the site database
+     */
+    TInfoShop getInfo(TInfoShop_Input shopInfo);
+
+    /**
+     * returns information about all existing shops from the site
+     * database
+     */
+    TInfoShop[] getAllInfo();
+
+    /**
+     * deletes a shop reference from the site database
+     */
+    void deleteShopRef(TShopRef shopRef);
+
+    /**
+     * set secondary domain names for a shop
+     */
+    void setSecondaryDomains(TShopRef shopRef, java.lang.String[] secondaryDomains);
+
+}
