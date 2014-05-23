a426a2078ba43a79ed070cb98500c0c86fc45c8f
Oliver Trosien <otrosien@epages.com>
2014-05-23 16:56:18 +0200

    inherit javadoc from interfaces, move customerservice4 to gen-old.

--- java/shop/src/main/java/de/epages/ws/product12/ProductServiceClientImpl.java
+++ java/shop/src/main/java/de/epages/ws/product12/ProductServiceClientImpl.java
@@ -28,55 +28,15 @@ public final class ProductServiceClientImpl implements ProductServiceClient {
         this.stub = factory.create(config, serviceLocator);
     }
 
-    /**
-     * Gets information about a list of Products. If there is an error with a
-     * single Product, then this Product will be skipped and it will be not part
-     * of there result list.
-     * 
-     * @param paths
-     *            list of Product paths
-     * @return list of TGetInfo_Return
-     * @throws RemoteException
-     * @see TGetInfo_Return
-     */
-    @Override
     public TGetInfo_Return[] getInfo(String[] products) {
         return getInfo(products, new String[] {});
     }
 
-    /**
-     * Gets information about a list of Products. If there is an error with a
-     * single Product, then this Product will be skipped and it will be not part
-     * of there result list.
-     * 
-     * @param paths
-     *            list of Product paths
-     * @param attributes
-     *            list of additional attributes
-     * @return list of TGetInfo_Return
-     * @throws RemoteException
-     * @see TGetInfo_Return
-     */
     @Override
     public TGetInfo_Return[] getInfo(String[] products, String[] attributes) {
         return getInfo(products, attributes, new String[] {});
     }
 
-    /**
-     * Gets information about a list of Products. If there is an error with a
-     * single Product, then this Product will be skipped and it will be not part
-     * of there result list.
-     * 
-     * @param paths
-     *            list of Product paths
-     * @param attributes
-     *            list of additional attributes
-     * @param languageCodes
-     *            list of language codes
-     * @return list of TGetInfo_Return
-     * @throws RemoteException
-     * @see TGetInfo_Return
-     */
     @Override
     public TGetInfo_Return[] getInfo(String[] products, String[] attributes, String[] languageCodes) {
         try {
@@ -86,18 +46,6 @@ public final class ProductServiceClientImpl implements ProductServiceClient {
         }
     }
 
-    /**
-     * Creates a list of Products with the given data. If there is an error with
-     * a single Product, then this Product will be skipped and it will be not
-     * part of there result list.
-     * 
-     * @param Products
-     *            list of TCreate_Input
-     * @return list of TCreate_Return
-     * @throws RemoteException
-     * @see TCreate_Input
-     * @see TCreate_Return
-     */
     @Override
     public TCreate_Return[] create(TCreate_Input[] products) {
         try {
@@ -107,18 +55,6 @@ public final class ProductServiceClientImpl implements ProductServiceClient {
         }
     }
 
-    /**
-     * Updates a list of Products with the given data. If there is an error with
-     * a single Product, then this Product will be skipped and it will be not
-     * part of there result list.
-     * 
-     * @param Products
-     *            list of TUpdate_Input
-     * @return list of TUpdate_Return
-     * @throws RemoteException
-     * @see TUpdate_Input
-     * @see TUpdate_Return
-     */
     @Override
     public TUpdate_Return[] update(TUpdate_Input[] products) {
         try {
@@ -128,17 +64,6 @@ public final class ProductServiceClientImpl implements ProductServiceClient {
         }
     }
 
-    /**
-     * Deletes a list of Products. If there is an error with a single Product,
-     * then this Product will be skipped and it will be not part of there result
-     * list.
-     * 
-     * @param paths
-     *            list of Product paths
-     * @return list of TDelete_Return
-     * @throws RemoteException
-     * @see TDelete_Return
-     */
     @Override
     public TDelete_Return[] delete(String[] products) {
         try {
@@ -148,17 +73,6 @@ public final class ProductServiceClientImpl implements ProductServiceClient {
         }
     }
 
-    /**
-     * Checks if a list of Products exists. If there is an error with a single
-     * Product, then this Product will be skipped and it will be not part of
-     * there result list.
-     * 
-     * @param paths
-     *            list of Product paths
-     * @return list of TExists_Return
-     * @throws RemoteException
-     * @see TExists_Return
-     */
     @Override
     public TExists_Return[] exists(String[] products) {
         try {
@@ -168,15 +82,6 @@ public final class ProductServiceClientImpl implements ProductServiceClient {
         }
     }
 
-    /**
-     * Searches for Products by given search parameters.
-     * 
-     * @param parameters
-     *            list of TFind_Input
-     * @return List of Product paths
-     * @throws RemoteException
-     * @see TFind_Input
-     */
     @Override
     public String[] find(TFind_Input searchParameters) {
         try {
