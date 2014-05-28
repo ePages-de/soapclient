46db0c83565d73fb18d18393f6c45060f388573a
Oliver Trosien <otrosien@epages.com>
2014-05-28 11:38:57 +0200

    EPG-33414 - remove multistore services for now, we'll pick them up later

--- java/multistore/src/main/java/de/epages/ws/multistoreconfig/MultiStoreConfigServiceStubFactory.java
+++
@@ -1,8 +0,0 @@
-package de.epages.ws.multistoreconfig;
-
-import de.epages.ws.WebServiceConfiguration;
-import de.epages.ws.multistoreconfig.stub.*;
-
-public interface MultiStoreConfigServiceStubFactory {
-    Port_MultiStoreConfig create(WebServiceConfiguration config, MultiStoreConfigService service);
-}
